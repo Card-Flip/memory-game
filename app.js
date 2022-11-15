e3c5ee982f4e61e5a88bbbd9dc4546c9da3267dd
Tyler Huntley <tylershuntley@hotmail.com>
2022-11-15 22:43:47 +0000

    rendering each image 1 time for a total of 16

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let score = [];
 
 //-------------DOM-------------//
 // let gameBoard = document.getElementById('gameBoard');
-// let parentEl = document.getElementById('imgContainer');
+let parentEl = document.getElementById('imgContainer');
 let gameBoard = document.getElementById('imgContainer');
 // let imgOne = document.getElementById('imgOne');
 // let imgTwo = document.getElementById('imgTwo');
@@ -30,73 +30,48 @@ let randomImageArray = [];
 function imageRandomizer() {
   while (randomImageArray.length < 16) {
     let randomImg = randomIndex();
-    if (!(randomImageArray.includes(imagesArray[randomIndex]))) {
-      randomImageArray.push(imagesArray[randomImg]);
+    if (!(randomImageArray.includes(randomImg))) {
+      randomImageArray.push(randomImg);
+      // else if (randomImageArray.includes(randomImg)) 
       // for (let i = 0; i < imagesArray.length; i++) {
       //   let randomImage = randomIndex();
-      //   if (randomImage) {
-
+      //     if (randomImageArray.includes(imagesArray(randomImg)) || imagesArray.includes(randomImg)) {
+      // }
+      // if (randomImageArray.includes(imagesArray[randomIndex])) { 
+      // }
     }
   }
 }
+console.log('random image array', randomImageArray);
 
 //------------Event Handlers-------------//
 function handleImageClick(event) {
-  let animalClick = event.target.src;
+  let animalClick = event.target.alt;
   for (let i = 0; i < imagesArray.length; i++) {
     if (imagesArray[i].name === animalClick) {
       score[i].clicks++;
     }
   }
 }
-console.log(score);
+console.log('score', score);
 
 //-------------Table Render-------------//
 function tableRender() {
   imageRandomizer();
-  let rowEl = document.createElement('tr');
-  let cardEl = document.createElement('td');
-  let imageEl = document.createElement('img');
-  for (let i = 0; i < imagesArray.length; i++) {
-    imageEl.src = imagesArray[i].imagePath;
-    cardEl.appendChild(imageEl);
-    rowEl.appendChild(cardEl);
+  let itemIndex = 0;
+  while (itemIndex < randomImageArray.length) {
+    let rowEl = document.createElement('tr');
+    for (let i = 0; i < 4; i++) {
+      let cardEl = document.createElement('td');
+      let imageEl = document.createElement('img');
+      imageEl.src = imagesArray[randomImageArray[itemIndex]].imagePath;
+      itemIndex++;
+      rowEl.appendChild(cardEl);
+      cardEl.appendChild(imageEl);
+    }
+    parentEl.appendChild(rowEl);
   }
-  console.log(imagesArray);
-
-  imgOne.src = randomImageArray[0].imagePath;
-  imgTwo.src = randomImageArray[1].imagePath;
-  imgThree.src = randomImageArray[2].imagePath;
-  imgFour.src = randomImageArray[3].imagePath;
-  imgFive.src = randomImageArray[4].imagePath;
-  imgSix.src = randomImageArray[5].imagePath;
-  imgSeven.src = randomImageArray[6].imagePath;
-  imgEight.src = randomImageArray[7].imagePath;
-  imgNine.src = randomImageArray[8].imagePath;
-  imgTen.src = randomImageArray[9].imagePath;
-  imgEleven.src = randomImageArray[10].imagePath;
-  imgTwelve.src = randomImageArray[11].imagePath;
-  imgThirteen.src = randomImageArray[12].imagePath;
-  imgFourteen.src = randomImageArray[13].imagePath;
-  imgFifteen.src = randomImageArray[14].imagePath;
-  imgSixteen.src = randomImageArray[15].imagePath;
-
-  imgOne.alt = randomImageArray[0].name;
-  imgTwo.alt = randomImageArray[1].name;
-  imgThree.alt = randomImageArray[2].name;
-  imgFour.alt = randomImageArray[3].name;
-  imgFive.alt = randomImageArray[4].name;
-  imgSix.alt = randomImageArray[5].name;
-  imgSeven.alt = randomImageArray[6].name;
-  imgEight.alt = randomImageArray[7].name;
-  imgNine.alt = randomImageArray[8].name;
-  imgTen.alt = randomImageArray[9].name;
-  imgEleven.alt = randomImageArray[10].name;
-  imgTwelve.alt = randomImageArray[11].name;
-  imgThirteen.alt = randomImageArray[12].name;
-  imgFourteen.alt = randomImageArray[13].name;
-  imgFifteen.alt = randomImageArray[14].name;
-  imgSixteen.alt = randomImageArray[15].name;
+  console.log('images array', imagesArray);
 }
 
 //-------------Objects-------------//
